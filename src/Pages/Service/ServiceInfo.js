3e4f6425a06970a2f46e0705b78d5b25a92a92e8
Pukkok <krystalgggg@gmail.com>
2025-06-12 01:20:18 +0900

    [Feat] react-merge-table 적용

--- src/Pages/Service/ServiceInfo.js
+++ src/Pages/Service/ServiceInfo.js
@@ -1,8 +1,29 @@
 import React from "react";
 import ImgBox from "../../Components/ImgBox";
 import HeadLine from "../Common/HeadLine";
+import { MergeTable, TableBody } from 'react-merge-table' 
 
 function ServiceInfo ({depth1, depth2}) {
+
+    const rows = [
+        ['수시(3)', 
+            ['식단표(선택)', '위반내용 및 조치 결과', '유치원 규칙'], 
+            "~"
+        ],
+        ['4월 정시 (19)', 
+            ['기관 기본현황', '원장명, 설립·경영자명', '교지·교사 등 시설현황', '연령별 학급 수·유아 수'], 
+            ['직위·자격별 교직원현황', '교사의 현 기관 근속연수', '교육과정 편성·운영에 관한 사항', '방과후 과정 편성·운영에 관한 사항', '수업일수 현황']
+        ],
+        [
+            '$',
+            ['교육과정비, 방과후 과정 운영비', '특성화 활동비', '유치원 회계 예산서', '급식실시 및 급식사고 발생·처리 현황', '환경위생관리 현황'],
+            ['안전점검 현황', '안전교육 계획 및 실시 현황', '공제회 및 보험가입 현황', '통학차량 운영 현황', '유치원 평가에 관한 사항']
+        ],
+        ['10월 정시(13)',
+            ['원장명, 설립·경영자명', '연령별 학급수·유아수', '직위·자격별 교원현황', '교사의 현 기관 근속연수', '교육과정비, 방과후 과정 운영비', '특성화 활동비'],
+            ['유치원 회계 결산서', '적립금 현황', '급식실시 및 급식사고 발생· 처리 현황', '환경위생관리 현황', '안전점검 현황', '공제회 및 보험가입 현황', '통학차량 운영 현황']
+        ]
+    ]
 
     return(
         <>
@@ -32,90 +53,32 @@ function ServiceInfo ({depth1, depth2}) {
                 <h5>2012년 9월부터 유치원정보공시제도 포털사이트(유치원알리미)가 개통되어 7개 항목, 23개(수시 3종, 정시 20종) 범위에서 매년 1회 이상 유치원의 주요 정보들이 공시되고 있습니다.</h5>
                 <h5>유치원에서는 공시 기준에 따라 유아 · 교직원 현황, 유치원 회계 현황, 환경위생 및 안전관리 사항 등 유치원의 주요 정보를 공시하고 있습니다.</h5>
             </div>
+
             <div className="info-table">
-                <table>
+                <MergeTable>
                     <colgroup>
                         <col style={{width:'20%'}}/>
                         <col style={{width:'40%'}}/>
                         <col style={{width:'40%'}}/>
                     </colgroup>
-                    <tbody>
-                        <tr>
-                            <th>수시<b>(3)</b></th>
-                            <td colSpan={2}>
+                    <TableBody 
+                        defaultStyle={false}
+                        rows={rows}
+                        columnRenderers={{
+                            0: cell => <th>{cell.content.label}</th>,
+                            1: (cell) => (
                                 <ul>
-                                    <li>식단표(선택)</li>
-                                    <li>위반내용 및 조치 결과</li>
-                                    <li>유치원 규칙</li>
+                                    {cell.contents.map((v) => <li key={v.key}>{v.label}</li>)}
                                 </ul>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th rowSpan={2}>4월 정시 <b>(19)</b></th>
-                            <td>
+                            ),
+                            2: cell => (
                                 <ul>
-                                    <li>기관 기본현황</li>
-                                    <li>원장명, 설립·경영자명</li>
-                                    <li>교지·교사 등 시설현황</li>
-                                    <li>연령별 학급 수·유아 수</li>
+                                    {cell.contents.map((v) => <li key={v.key}> {v.label}</li>)}
                                 </ul>
-                            </td>
-                            <td>
-                                <ul>
-                                    <li>직위·자격별 교직원현황</li>
-                                    <li>교사의 현 기관 근속연수</li>
-                                    <li>교육과정 편성·운영에 관한 사항</li>
-                                    <li>방과후 과정 편성·운영에 관한 사항</li>
-                                    <li>수업일수 현황</li>
-                                </ul>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <ul>
-                                    <li>교육과정비, 방과후 과정 운영비</li>
-                                    <li>특성화 활동비</li>
-                                    <li>유치원 회계 예산서</li>
-                                    <li>급식실시 및 급식사고 발생·처리 현황</li>
-                                    <li>환경위생관리 현황</li>
-                                </ul>
-                            </td>
-                            <td>
-                                <ul>
-                                    <li>안전점검 현황</li>
-                                    <li>안전교육 계획 및 실시 현황</li>
-                                    <li>공제회 및 보험가입 현황</li>
-                                    <li>통학차량 운영 현황</li>
-                                    <li>유치원 평가에 관한 사항</li>
-                                </ul>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>10월 정시<b>(13)</b></th>
-                            <td>
-                                <ul>
-                                    <li>원장명, 설립·경영자명</li>
-                                    <li>연령별 학급수·유아수</li>
-                                    <li>직위·자격별 교원현황</li>
-                                    <li>교사의 현 기관 근속연수</li>
-                                    <li>교육과정비, 방과후 과정 운영비</li>
-                                    <li>특성화 활동비</li>
-                                </ul>
-                            </td>
-                            <td>
-                                <ul>
-                                    <li>유치원 회계 결산서</li>
-                                    <li>적립금 현황</li>
-                                    <li>급식실시 및 급식사고 발생· 처리 현황</li>
-                                    <li>환경위생관리 현황</li>
-                                    <li>안전점검 현황</li>
-                                    <li>공제회 및 보험가입 현황</li>
-                                    <li>통학차량 운영 현황</li>
-                                </ul>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                            ) 
+                        }}
+                    />
+                </MergeTable>
             </div>
         </div>
         </>
